1ae6f35865e589bea8e514cf51f9da4ccebe718d
Norman Zhi <yz39g13@soton.ac.uk>
2015-02-24 10:22:19 +0000

    Draft interconnect communcation protocol

---
+++ inc/communication.h
@@ -0,0 +1,28 @@
+#ifndef COMMUNICATION_H
+#define COMMUNICATION_H
+
+// UART baudrate
+#define BAUD	
+
+// Response
+#define COM_ACK		0
+// End of variable length response
+#define COM_END		0xFF
+
+// Data structure: Type, Data
+// No data, response {COM_ACK} indicates IlMatto2 exist
+#define COM_PING	0
+// Wakeup wireless module, response {COM_ACK}
+#define COM_WAKEUP	1
+// Suspend wireless module for power saving, response {COM_ACK}
+#define COM_SUSPEND	2
+
+// Wireless connection operations
+// Ping for other end, no data, response {COM_ACK} for success, {COM_END} for timeout
+#define COM_W_PING	100
+// Start sending & receiving ADC data, no data, response {COM_ACK}
+#define COM_W_ADC	101
+// Send data to other end, data {Length(2 bytes), Data}, response {COM_ACK}
+#define COM_W_SEND	102
+
+#endif
