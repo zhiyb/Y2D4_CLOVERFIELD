90813e5661d0a4046c08575416e9de1cadbcf52d
Norman Zhi <yz39g13@soton.ac.uk>
2015-03-04 17:27:39 +0000

    UART communication protocol updates

--- inc/communication.h
+++ inc/communication.h
@@ -1,9 +1,13 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <inttypes.h>
 
-// UART baudrate (750,000 bps)
+// UART baudrate (750kbps)
 #define BAUD		(F_CPU / 8 / (1 + 1))
 // UART data buffer size
 #define BUFFER_SIZE	64
@@ -17,11 +21,11 @@
 
 // Data structure: Command, Data
 // No data, response {COM_ACK} indicates IlMatto2 exist
-#define COM_PING	0
+#define COM_PING	50
 // Wakeup wireless module, response {COM_ACK}
-#define COM_WAKEUP	1
+#define COM_WAKEUP	51
 // Suspend wireless module for power saving, response {COM_ACK}
-#define COM_SUSPEND	2
+#define COM_SUSPEND	52
 
 // Wireless connection operations
 // Ping for other end, no data, response {COM_ACK} for success, {COM_END} for timeout
@@ -34,12 +38,16 @@
 #define COM_W_SEND	103
 
 // Data received, data {Length(1 byte), Data}, response {COM_ACK}
-#define COM_W_RECV	200
+#define COM_W_RECV	150
 
 // Both send & receive can use the same buffering package type
 struct package_t {
 	uint8_t command, length, valid;
 	uint8_t data[BUFFER_SIZE];
 };
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
