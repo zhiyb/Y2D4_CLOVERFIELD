55414f0cdf42eb078ae41487827787c8f8367160
Norman Zhi <yz39g13@soton.ac.uk>
2015-03-04 18:09:41 +0000

    Scroll list use namespace menu now

--- list/menufunc.cpp
+++ list/menufunc.cpp
@@ -1,7 +1,19 @@
 #include <avr/io.h>
 #include "menu.h"
 
-bool func_toggle(void)
+namespace menu
+{
+	static tft_t *tft;
+}
+
+using namespace menu;
+
+void menu::setTFT(tft_t *t)
+{
+	tft = t;
+}
+
+bool menu::toggle::func(void)
 {
 	PINB |= _BV(7);
 	return false;
