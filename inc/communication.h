c4b8c51d78a54960a1a0b6a9abce5496b31e1a3c
Norman Zhi <yz39g13@soton.ac.uk>
2015-03-05 02:48:37 +0000

    UART protocal modifies

--- inc/communication.h
+++ inc/communication.h
@@ -14,31 +14,37 @@ extern "C" {
 
 // Response
 #define COM_ACK		0
-// End of variable length response
+// End of variable length response / command failed
 #define COM_END		0xFF
 
 // From Il Matto 1
 
+// Data masks
+// Data type mask
+#define COM_TYPE	0x0F
+// Variable length data (length byte & data)
+#define COM_DATA	0x10
+
 // Data structure: Command, Data
 // No data, response {COM_ACK} indicates IlMatto2 exist
-#define COM_PING	50
+#define COM_PING	1
 // Wakeup wireless module, response {COM_ACK}
-#define COM_WAKEUP	51
+#define COM_WAKEUP	2
 // Suspend wireless module for power saving, response {COM_ACK}
-#define COM_SUSPEND	52
+#define COM_SUSPEND	3
 
 // Wireless connection operations
 // Ping for other end, no data, response {COM_ACK} for success, {COM_END} for timeout
-#define COM_W_PING	100
+#define COM_W_PING	4
 // Start sending & receiving sound data, no data, response {COM_ACK}
-#define COM_W_SOUND	101
+#define COM_W_SOUND	5
 // Stop sending & receiving sound data, no data, response {COM_ACK}
-#define COM_W_SOUND_END	102
+#define COM_W_SOUND_END	6
 // Send data to other end, data {Length(1 byte), Data}, response {COM_ACK}
-#define COM_W_SEND	103
+#define COM_W_SEND	(COM_DATA | 7)
 
 // Data received, data {Length(1 byte), Data}, response {COM_ACK}
-#define COM_W_RECV	150
+#define COM_W_RECV	(COM_DATA | 8)
 
 // Both send & receive can use the same buffering package type
 struct package_t {
