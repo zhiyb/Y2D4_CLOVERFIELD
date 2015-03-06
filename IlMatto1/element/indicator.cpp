d56ba37521720526be99d6cf10ba7ee2d8edd4ee
Norman Zhi <yz39g13@soton.ac.uk>
2015-03-06 03:02:18 +0000

    IlMatto1 Connection indicator update

--- IlMatto1/element/indicator.cpp
+++ IlMatto1/element/indicator.cpp
@@ -3,8 +3,6 @@
 
 namespace indicator
 {
-	static void refresh(uint16_t clr);
-
 	// Ping related
 	static volatile bool pingChk;
 	static void ping(void);
@@ -16,18 +14,26 @@ void indicator::init(void)
 	pingChk = false;
 }
 
-static void indicator::refresh(uint16_t clr)
+void indicator::refresh(uint16_t clr, const char *str)
 {
 	tft.rectangle(0, 0, INDICATOR_SIZE, INDICATOR_SIZE, clr);
+	tft.rectangle(INDICATOR_SIZE, 0, tft.width() - INDICATOR_SIZE, INDICATOR_SIZE, Black);
+	tft.setZoom(1);
+	tft.setForeground(clr);
+	tft.setBackground(Black);
+	tft.setXY(INDICATOR_SIZE + INDICATOR_SIZE / 2, 0);
+	tft.putString(str, true);
 }
 
 static void indicator::ping(void)
 {
 	if (pingChk)
 		return;
+	PINB |= _BV(7);
 	package_t *pkg = uart0_txPackage();
 	if (!pkg) {
 		pingChk = false;
+		refresh(Magenta, PSTR("Pending tx package..."));
 		return;
 	}
 	pkg->command = COM_PING;
@@ -41,18 +47,18 @@ static void indicator::pingCheck(void)
 	if (!pingChk)
 		return;
 	if (uart0_ack())
-		refresh(Blue);
+		refresh(Blue, PSTR(""));
 	else {
 		uart0_reset();
-		refresh(Red);
+		refresh(Red, PSTR("Il Matto 2 not exist!"));
 	}
 	pingChk = false;
 }
 
 void indicator::pool(void)
 {
-	if (tick() == 100)
+	if (tick() <= 100)
 		ping();
-	else if (tick() == 110)
+	else if (tick() >= 101)
 		pingCheck();
 }
--- IlMatto1/element/indicator.h
+++ IlMatto1/element/indicator.h
@@ -3,10 +3,15 @@
 
 #define INDICATOR_SIZE	LIST_TOP_RESERVED
 
+#include <inttypes.h>
+
 namespace indicator
 {
 	void init(void);
+	// Pooling for ping
 	void pool(void);
+	// String in PROGMEM
+	void refresh(uint16_t clr, const char *str);
 }
 
 #endif
