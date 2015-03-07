7e1c43f89ca2d9f947a4934eb94fd9d02a003c93
zhiyb <yz39g13@soton.ac.uk>
2015-03-07 13:56:12 +0000

    Wrapped shifting reference (ak12g12)

--- en_decryption.c
+++ en_decryption.c
@@ -3,8 +3,9 @@
  *
  *  Created on: 7 Mar 2015
  *      Author: ak12g12
+	idea of wrapped shifting from
+	http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3990.pdf
  */
-
 
 uint8_t encryption(uint8_t data)
 {
