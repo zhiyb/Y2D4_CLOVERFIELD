c74b9ee2301b76efc9aade1ab9cdf10b946f49e2
Norman Zhi <yz39g13@soton.ac.uk>
2015-03-08 00:29:42 +0000

    UART communication protocal updates

--- inc/communication.h
+++ inc/communication.h
@@ -28,23 +28,29 @@ extern "C" {
 // Data structure: Command, Data
 // No data, response {COM_ACK} indicates IlMatto2 exist
 #define COM_PING	1
-// Wakeup wireless module, response {COM_ACK}
+// Wakeup wireless module
 #define COM_WAKEUP	2
-// Suspend wireless module for power saving, response {COM_ACK}
+// Suspend wireless module for power saving
 #define COM_SUSPEND	3
 
 // Wireless connection operations
-// Ping for other end, no data, response {COM_ACK} for success, {COM_END} for timeout
+// Ping for other end, no data
 #define COM_W_PING	4
-// Start sending & receiving sound data, no data, response {COM_ACK}
-#define COM_W_SOUND	5
-// Stop sending & receiving sound data, no data, response {COM_ACK}
-#define COM_W_SOUND_END	6
-// Send data to other end, data {Length(1 byte), Data}, response {COM_ACK}
-#define COM_W_SEND	(COM_DATA | 7)
+// Ping success reply
+#define COM_W_PING_SU	5
+// Ping timeout reply
+#define COM_W_PING_TO	6
+// Timeout for remote ping, unit: ms
+#define COM_W_PING_TIMEOUT	100
+// Start sending & receiving sound data, no data
+#define COM_W_SOUND	7
+// Stop sending & receiving sound data, no data
+#define COM_W_SOUND_END	8
+// Send data to other end, data {Length(1 byte), Data}
+#define COM_W_SEND	(COM_DATA | 20)
 
-// Data received, data {Length(1 byte), Data}, response {COM_ACK}
-#define COM_W_RECV	(COM_DATA | 8)
+// Data received, data {Length(1 byte), Data}
+#define COM_W_RECV	(COM_DATA | 21)
 
 // Both send & receive can use the same buffering package type
 struct package_t {
